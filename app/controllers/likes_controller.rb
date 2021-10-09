275e73f785e4daaf91e1b6dd87f004107c2e9e1e
hognjun95 <hongjungjeong95@gmail.com>
2021-10-09 11:38:44 +0900

    Create unlike_item action

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -46,7 +46,34 @@ class LikesController < ApiController
         end
     end
 
-    def update
-        
+    def unlike_item
+        begin
+            item_id = params[:item_id]
+            current_user = current_api_user
+
+            item = Item.find(item_id)
+        rescue => exception
+            puts "Error #{exception.class}!"
+            render json: {
+                ok: false,
+                error: "Item doesn't exist" 
+            } and return
+        else
+            begin
+                like = Like.find_by(user_id: current_user.id)
+
+                like.items = like.items.select {|item| item.id != item_id.to_i}
+
+                render json: {
+                    ok: true
+                }
+            rescue => exception
+                puts "Error #{exception.class}!"
+                render json: {
+                    ok: false,
+                    error: "Can't find the like list" 
+                } and return
+            end
+        end
     end
 end
