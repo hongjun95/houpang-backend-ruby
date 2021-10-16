c968226fd061407bb98d0ed142c01f453480d421
hongjun95 <hongjunjeong95@gmail.com>
2021-10-16 14:36:15 +0900

    Add created_at to ReviewEachSerializer

--- app/serializers/review_each_serializer.rb
+++ app/serializers/review_each_serializer.rb
@@ -1,5 +1,5 @@
 class ReviewEachSerializer < Panko::Serializer
-  attributes :id, :user_id, :item_id, :content, :rating
+  attributes :id, :user_id, :item_id, :content, :rating, :created_at
 
   has_one :item, serializer: ItemSerializer
   has_one :commenter, serializer: UserSerializer
